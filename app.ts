abc1f5dce541036590516d091796f7e3689af11c
j-koziel <jonathankoziel1@gmail.com>
2023-08-10 10:37:55 +0100

    feat: create issues and pulls getter functions

--- app.ts
+++ app.ts
@@ -10,11 +10,10 @@ const program: Command = new Command();
 
 program
   .version(`${pjson.version}`)
-  .description("A CLI which will create Issues and Pulls reports from an organisations repositories").option("-i, --issues <value>", "Create issues report for an organisation"
-  ).option("-p, --pulls <value>", "Create pulls report for an organisation")
+  .description("A CLI which will create Issues and Pulls reports from an organisations repositories")
+  .option("-i, --issues <value>", "Create issues report for an organisation")
+  .option("-p, --pulls <value>", "Create pulls report for an organisation")
   .parse(process.argv);
-
-const options = program.opts();
 
 console.log(figlet.textSync("Repo Reporter"));
 program.outputHelp();
--- reportsCreator.ts
+++ reportsCreator.ts
@@ -1,5 +1,10 @@
 import { Octokit } from "octokit"
+import { OptionValues } from "commander"
 import dotenv from "dotenv"
+
+import program from "./app"
+const options: OptionValues = program.opts()
+
 
 dotenv.config()
 
@@ -8,22 +13,35 @@ const octokit = new Octokit({
   auth: process.env.PERSONAL_ACCESS_TOKEN
 })
 
-const issues = await octokit.request("GET /repos/twitter/the-algorithm/issues", {
-  owner: "twitter",
-  repo: "the-algorithm",
-  headers: {
-    "X-Github-Api-Version": "2022-11-28"
+
+async function getIssues(repo: string, owner: string) {
+  try {
+    const issues = await octokit.request(`GET /repos/${repo}/${owner}/issues`, {
+      owner,
+      repo,
+      headers: {
+        "X-Github-Api-Version": "2022-11-28"
+      }
+    });
+
+    console.log(issues)
+  } catch (err) {
+    console.log("An error occured while trying to retreive the issues!", err)
   }
-});
+}
 
-console.log(issues)
+async function getPulls(repo: string, owner: string): Promise<void> {
+  try {
+    const pulls = await octokit.request(`GET /repos/${repo}/${owner}/pulls`, {
+      owner,
+      repo,
+      headers: {
+        "X-Github-Api-Version": "2022-11-28"
+      }
+    });
 
-const pulls = await octokit.request('GET /repos/twitter/the-algorithm/pulls', {
-  owner: 'OWNER',
-  repo: 'REPO',
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
+    console.log(pulls)
+  } catch (err) {
+    console.log("An error occured while trying to retreive the issues!", err)
   }
-})
-
-console.log(pulls)
+}
