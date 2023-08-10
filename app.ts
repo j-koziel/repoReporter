4c1db79b2fea1a61596ca43c953fccec91b685c7
j-koziel <jonathankoziel1@gmail.com>
2023-08-10 10:38:48 +0100

    feat(cli): modify cli to support repo owners and names

--- app.ts
+++ app.ts
@@ -11,8 +11,8 @@ const program: Command = new Command();
 program
   .version(`${pjson.version}`)
   .description("A CLI which will create Issues and Pulls reports from an organisations repositories")
-  .option("-i, --issues <value>", "Create issues report for an organisation")
-  .option("-p, --pulls <value>", "Create pulls report for an organisation")
+  .option("-i, --issues <value> <value>", "Create issues report for an organisation")
+  .option("-p, --pulls <value> <value>", "Create pulls report for an organisation")
   .parse(process.argv);
 
 console.log(figlet.textSync("Repo Reporter"));
